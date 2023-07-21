537b76712d67a0e3dc11df152e8e111952ac7b31
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-20 23:14:37 -0700

    Update PalindromeNum.java

--- PalindromeNum.java
+++ PalindromeNum.java
@@ -16,6 +16,7 @@ Input: x = -121
 Output: false
 Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 
+ -- Using String Conversion
 class Solution {
    //Tc : O(n)
     public boolean isPalindrome(int x)
@@ -33,4 +34,23 @@ class Solution {
         return true;
         
     }
+}
+
+-- Using Reverse 
+class Solution {
+    public boolean isPalindrome(int x)
+    {
+        int rev = 0;
+        int num = x;
+        int r;
+        while(num > 0)
+        {
+            r = num%10;
+            rev = rev * 10 + r;
+            num = num/10;
+        }
+        if(x == rev) return true;
+        else return false;
+    }
+
 }
