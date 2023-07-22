9d0b51543cf88f47a9e25e63adce07ce4e874dca
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-21 23:08:26 -0700

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -4,3 +4,28 @@ If there is no common prefix, return an empty string "".
 Example 1:
 Input: strs = ["flower","flow","flight"]
 Output: "fl"
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+         if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String baseString = strs[0];
+        StringBuilder prefix = new StringBuilder();
+
+        for (int i = 0; i < baseString.length(); i++) {
+            char ch = baseString.charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || ch != strs[j].charAt(i)) {
+                    return prefix.toString();
+                }
+            }
+            prefix.append(ch);
+        }
+
+        return prefix.toString();
+
+        
+    }
+}
