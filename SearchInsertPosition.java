ea7c40c5b85871fffe53ee17baaf4795399adc6d
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-22 22:49:35 -0700

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -15,3 +15,20 @@ Example 3:
 
 Input: nums = [1,3,5,6], target = 7
 Output: 4
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid -1;
+            else low = mid + 1;
+        }
+        int mid = (low + high)/2;
+        if(target > nums[mid]) return mid + 1;
+        return mid;
+    }
+}
