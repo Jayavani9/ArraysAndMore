2144dd6aeb2509f1b81bcefd7736bf612b778815
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-25 15:31:39 -0700

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,3 +1,4 @@
+/*
 35. Search Insert Position
 
 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
@@ -15,6 +16,7 @@ Example 3:
 
 Input: nums = [1,3,5,6], target = 7
 Output: 4
+*/
 
 class Solution {
     //Tc : O(log n) and Sc: O(1)
