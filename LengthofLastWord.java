bf1ec988778c630b06ff733629c75da865df36c4
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-25 14:33:38 -0700

    Update LengthofLastWord.java

--- LengthofLastWord.java
+++ LengthofLastWord.java
@@ -1,3 +1,4 @@
+/*
 Given a string s consisting of words and spaces, return the length of the last word in the string.
 
 A word is a maximal substring consisting of non-space characters only.
@@ -10,8 +11,7 @@ Example 2:
 Input: s = "   fly me   to   the moon  "
 Output: 4
 Explanation: The last word is "moon" with length 4.
-
-
+*/
   
 class Solution {
   //Tc: O(n+m) where n is the length of the String and m is the loop (no.of words in the input string) ; Sc: O(n)
