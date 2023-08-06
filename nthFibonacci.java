8fa66034e44a8dbf994186fdf7f644ad595a9ee9
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-06 08:48:32 -0500

    Update nthFibonacci.java

--- nthFibonacci.java
+++ nthFibonacci.java
@@ -1,1 +1,18 @@
+The Fibonacci sequence is a series of numbers defined as follows: the first number of the sequence is 0, the second number is 1, and each subsequent number is the sum of the two preceding numbers. For example, the sequence starts as 0, 1, 1, 2, 3, 5, 8, and so on.
 
+Write a function that takes an integer n as input and returns the nth Fibonacci number in the sequence.
+
+Important note: In this question, the indexing of the Fibonacci sequence is slightly different. The first Fibonacci number is denoted as F0, the second number as F1, and so on. Therefore, calling getNthFib(1) should return F0, getNthFib(2) should return F1, and so forth.
+
+Sample Input #1
+n = 2
+    
+Sample Output #1
+1 // 0, 1
+    
+Sample Input #2
+n = 6
+    
+Sample Output #2
+5 // 0, 1, 1, 2, 3, 5
+    
