273c9221f86b02a0b0360c91ccabd35a04b30a97
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-06 08:55:08 -0500

    Update ShiftsTheLetters.java

--- ShiftsTheLetters.java
+++ ShiftsTheLetters.java
@@ -1,1 +1,11 @@
+Write a function that takes a non-empty string of lowercase letters and a non-negative integer as input. 
+The function should return a new string where each letter in the input string is shifted by a given number of positions in the alphabet. 
+If the shift exceeds the letter 'z', it should wrap around to the letter 'a'.
 
+Sample Input
+string = "xyz"
+key = 2
+    
+Sample Output
+"zab"
+    
