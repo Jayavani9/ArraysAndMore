e2b8edbc7853a8861d948d521bd71a837cf249fc
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-08 14:20:01 -0500

    Update threeNumAdd.java

--- threeNumAdd.java
+++ threeNumAdd.java
@@ -1,1 +1,14 @@
+Create a function that accepts a non-empty array of distinct integers and an integer representing a target sum. 
+The function's purpose is to identify and return a two-dimensional array containing all the triplets from the input array that add up to the target sum. 
+Each triplet should consist of three distinct numbers, and the numbers within each triplet should be in ascending order.
+The triplets themselves should be ordered in ascending order based on the numbers they contain.
+
+If no combination of three numbers from the array adds up to the target sum, the function should return an empty array.
+
+Sample Input
+array = [12, 3, 1, 2, -6, 5, -8, 6]
+targetSum = 0
+Sample Output
+[[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+
 
