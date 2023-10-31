8c42e853ff2f176879e23c7ed4b9588218244bf7
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-10-31 16:53:41 -0700

    Update ProductofArrayExceptSelf.java

--- ProductofArrayExceptSelf.java
+++ ProductofArrayExceptSelf.java
@@ -35,3 +35,33 @@ class Solution {
         return res;
     }
 }
+
+
+/*
+
+//Maintaining two sep arrays for left and right products
+int res[] = new int[nums.length];
+int left[] = new int[nums.length];
+int right[] = new int[nums.length];
+left[0] = 1;
+right[nums.length-1] = 1;
+
+for(int i = 1 ; i < nums.length; i++)
+{
+    left[i] = nums[i-1] * left[i-1];
+
+}
+
+for(int j = nums.length-2; j>=0 ; j--)
+{
+    right[j] = right[j+1] * nums[j+1];
+}
+
+for(int i = 0 ; i < nums.length;i++)
+{
+    res[i] = left[i] * right[i];
+}
+return res;
+}
+}
+*/
