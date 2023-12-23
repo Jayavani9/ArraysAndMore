4341561e32fa3adf44bc5152c88389ac8bae85ad
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-12-22 20:27:53 -0500

    Update maxSubArr.java

--- maxSubArr.java
+++ maxSubArr.java
@@ -58,3 +58,18 @@ class Solution {
         */
     }
 }
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0 ; i < nums.length; i++)
+        {
+            sum += nums[i];
+            if(sum > maxSum) maxSum = sum;
+            if(sum < 0) sum = 0;
+        }
+        return maxSum;
+    }
+}
