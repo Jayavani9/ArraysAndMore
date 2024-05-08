89ed7d6ecbc5201d7f18939df13d1c9479c124bc
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-05-07 23:03:05 -0700

    Create Sort0s1s2s.java

---
+++ Sort0s1s2s.java
@@ -0,0 +1,92 @@
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+Example 2:
+
+Input: 
+N = 3
+arr[] = {0 1 0}
+Output:
+0 0 1
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int zerocnt = 0;
+        int onecnt = 0;
+        int twocnt = 0;
+        
+        // code here 
+        
+        for(int i = 0; i < a.length; i++)
+        {
+            if(a[i] == 0) zerocnt++;
+            if(a[i] == 1) onecnt++;
+            if(a[i] == 2) twocnt++;
+        }
+        
+        for(int i = 0; i < zerocnt; i++) a[i] = 0;
+        for(int i = zerocnt; i < zerocnt + onecnt; i++) arr[i] = 1;
+        for(int i = onecnt + zerocnt; i < n; i++) arr[i] = 2;
+        
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+
+// } Driver Code Ends
