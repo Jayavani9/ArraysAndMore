908dc05988d0d1979683d751e3e2bac6e7538ce8
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-06-22 13:56:58 -0700

    Update kDiffPairs.java

--- kDiffPairs.java
+++ kDiffPairs.java
@@ -1,23 +1,26 @@
-//https://leetcode.com/problems/k-diff-pairs-in-an-array/
-
 class Solution {
-  
     public int findPairs(int[] nums, int k) {
         //Tc: O(n) Sc: O(n)
-        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
         int cnt = 0;
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++)
+        {
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
-        for(int key : map.keySet()){
-            if(k == 0 && map.get(key) > 1){
+
+        for(int key: map.keySet())
+        {
+            if(k > 0 && map.containsKey(k+key))
+            {
                 cnt++;
             }
-            else if(k > 0 && map.containsKey(key + k)){
+
+            if(k == 0 && map.get(key) > 1)
+            {
                 cnt++;
             }
         }
         return cnt;
-        
     }
 }
