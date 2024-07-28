b7cabd075da7b4b49035ccb7d32b33c665e65dc0
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-07-27 23:53:02 -0700

    Update RectangleOverlap.java

--- RectangleOverlap.java
+++ RectangleOverlap.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        //Tc: O(n) Sc: O(1) 
+        //Tc: O(1) Sc: O(1) 
         return rec2[1] < rec1[3] && rec2[0] < rec1[2] && rec1[1] < rec2[3] && rec1[0] < rec2[2];
     }
 }
