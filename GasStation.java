2d92cc8040b467d2807766921e53855525ccc3d6
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-07-30 22:22:40 -0700

    Update GasStation.java

--- GasStation.java
+++ GasStation.java
@@ -12,6 +12,7 @@ Example 1:
 
 Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
 Output: 3
+    
 Explanation:
 Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
 Travel to station 4. Your tank = 4 - 1 + 5 = 8
@@ -20,6 +21,7 @@ Travel to station 1. Your tank = 7 - 3 + 2 = 6
 Travel to station 2. Your tank = 6 - 4 + 3 = 5
 Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
 Therefore, return 3 as the starting index.
+    
 Example 2:
 
 Input: gas = [2,3,4], cost = [3,4,3]
@@ -32,7 +34,7 @@ Travel to station 1. Your tank = 3 - 3 + 3 = 3
 You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
 Therefore, you can't travel around the circuit once no matter where you start.
 
-
+//Using for loops
 class Solution {
     //Tc: O(N) Sc: O(1)
     public int canCompleteCircuit(int[] gas, int[] cost) {
