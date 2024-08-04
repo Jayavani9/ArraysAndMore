73fcfbd9ffd90884d9506570b51f9d1a1555542c
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-08-03 22:58:35 -0700

    Create revBits.java

---
+++ revBits.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/reverse-bits/description/
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+
+        int ans = 0; 
+        
+        for (int i = 0; i < 32; i++) {
+            ans = (ans << 1) | (n & 1); 
+            n >>= 1; 
+        }
+        
+        return ans;  
+    }
+}
